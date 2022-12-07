aeeb28e472360c86415924730bffa1e8c23a3152
impalainn <ahonavictor@gmail.com>
2022-12-07 01:07:29 -0800

    uppercase!

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        unicode = ord(str[i])
+        if unicode >= 97 and unicode <= 122:
+            unicode = unicode - 32
+        print("{}".format(chr(unicode)), end='')
+    print()
