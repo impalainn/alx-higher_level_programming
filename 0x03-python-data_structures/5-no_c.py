bdd6355b686dba7b93e8f0e3ecd2b109b86f63e2
impalainn <ahonavictor@gmail.com>
2022-12-19 21:53:38 -0800

    remove chr

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,5 @@ def no_c(my_string):
     '''
         function to exclude all 'c' and 'C'
     '''
-    x = my_string.copy()
-    for i in x:
-        if i != 'c' and i != 'C':
-            return(x)
+    copy = [i for i in my_string if i != 'c' and i != 'C']
+    return("".join(copy))
